f3f4eecb9318bb13b05cf7711a4b7a38b8866c36
Stig Dahl <stig@charlottendal.net>
2024-10-02 01:02:05 +0200

    Get a basic response

---
+++ forecast.py
@@ -0,0 +1,36 @@
+# Name:forecast
+
+# Author: Stig Dahl <stig@charlottendal.net>
+# Created: 2024-10-02
+
+'''
+Use YR-NO to lookup the weather forecast.
+
+TODO:
+- Add UserAgent pointing to repo: "AcmeWeatherApp/0.9 github.com/acmeweatherapp"
+- Add location
+- Read .env file
+- Cache data locally
+- Create directory structure at initial startup
+'''
+
+# Import modules
+import requests
+from pathlib import Path
+import json
+
+EXAMPLE_URI='https://api.met.no/weatherapi/locationforecast/2.0/compact'
+EXAMPLE_PAYLOAD = {"lat": 60.10, "lon": 9.58}
+USER_AGENT = "Forecast/0.1.0 github.com/sdaaish/forecast"
+headers = {"User-Agent": USER_AGENT}
+
+# Define main function
+def main():
+    r = requests.get(EXAMPLE_URI, params=EXAMPLE_PAYLOAD,headers=headers)
+#    r = requests.get('https://api.met.no/weatherapi/locationforecast/2.0/status',headers=headers)
+    print(r.url)
+    print(r.status_code)
+    print(r.json())
+
+if __name__ == "__main__":
+    main()
