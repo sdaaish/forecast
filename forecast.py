0b39f82b78968c0c400f3089273773ca3910bd9f
Stig Dahl <stig@charlottendal.net>
2024-10-02 01:45:44 +0200

    Extract the json response

--- forecast.py
+++ forecast.py
@@ -4,7 +4,7 @@
 # Created: 2024-10-02
 
 '''
-Use YR-NO to lookup the weather forecast.
+Using data from YR.NO (MET Norway) to lookup the weather forecast.
 
 TODO:
 - Add location
@@ -12,6 +12,9 @@ TODO:
 - Cache data locally
 - Create directory structure at initial startup
 - Error handling
+- Add credits
+- Follow Expires header, If-Modified-Since
+- Save an example to disk, use that for data processing and tests
 '''
 
 # Import modules
@@ -30,7 +33,12 @@ def main():
 #    r = requests.get('https://api.met.no/weatherapi/locationforecast/2.0/status',headers=headers)
     print(r.url)
     print(r.status_code)
-    print(r.json())
+    json= r.json()
+    meta= json["properties"]["meta"]
+    coordinates = json["geometry"]["coordinates"]
+    instant = json["properties"]["timeseries"][0]
+    print(meta,coordinates)
+    print(f'{instant["time"]}, {instant["data"]["instant"]["details"]}')
 
 if __name__ == "__main__":
     main()
